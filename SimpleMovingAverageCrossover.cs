69b064551c1636b0d92f5af5c388215723720eb8
Jacob Amaral <jake0303@hotmail.ca>
2020-02-08 11:13:47 -0500

    Added simple moving average crossover script

---
+++ SimpleMovingAverageCrossover.cs
@@ -0,0 +1,99 @@
+#region Using declarations
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Xml.Serialization;
+using NinjaTrader.Cbi;
+using NinjaTrader.Gui;
+using NinjaTrader.Gui.Chart;
+using NinjaTrader.Gui.SuperDom;
+using NinjaTrader.Gui.Tools;
+using NinjaTrader.Data;
+using NinjaTrader.NinjaScript;
+using NinjaTrader.Core.FloatingPoint;
+using NinjaTrader.NinjaScript.Indicators;
+using NinjaTrader.NinjaScript.DrawingTools;
+#endregion
+
+//This namespace holds Strategies in this folder and is required. Do not change it. 
+namespace NinjaTrader.NinjaScript.Strategies
+{
+	public class SimpleMovingAverageCrossover : Strategy
+	{
+		
+		private SMA fastSMA;
+		private SMA slowSMA;
+		
+		protected override void OnStateChange()
+		{
+			if (State == State.SetDefaults)
+			{
+				Description									= @"This strategy will trade when the 5 simple moving average crosses the 20 simple moving average.";
+				Name										= "SimpleMovingAverageCrossover";
+				Calculate									= Calculate.OnBarClose;
+				EntriesPerDirection							= 1;
+				EntryHandling								= EntryHandling.AllEntries;
+				IsExitOnSessionCloseStrategy				= false;
+				ExitOnSessionCloseSeconds					= 30;
+				IsFillLimitOnTouch							= true;
+				MaximumBarsLookBack							= MaximumBarsLookBack.Infinite;
+				OrderFillResolution							= OrderFillResolution.Standard;
+				Slippage									= 0;
+				StartBehavior								= StartBehavior.WaitUntilFlat;
+				TimeInForce									= TimeInForce.Gtc;
+				TraceOrders									= false;
+				RealtimeErrorHandling						= RealtimeErrorHandling.StopCancelClose;
+				StopTargetHandling							= StopTargetHandling.PerEntryExecution;
+				BarsRequiredToTrade							= 20;
+				Fast = 5;
+				Slow = 20;
+				// Disable this property for performance gains in Strategy Analyzer optimizations
+				// See the Help Guide for additional information
+				IsInstantiatedOnEachOptimizationIteration	= true;
+			}
+			if (State == State.Configure)
+			{
+				fastSMA = SMA(Fast);
+				slowSMA = SMA(Slow);
+				
+				//Anytime we are in a live position, we have a 100$ take profit
+				SetProfitTarget(CalculationMode.Currency, 100);
+				
+				//Set a stop loss for 50$
+				SetStopLoss(CalculateMode.Currency, 50);
+
+			}
+		}
+
+		protected override void OnBarUpdate()
+		{
+			if (BarsInProgress != 0) 
+				return;
+			
+			
+			//Enter a long trade when the 5 simple moving average crosses the 20 simple moving average
+			if (CrossAbove(fastSMA, slowSMA, 1))
+				EnterLong();
+			
+		}
+		
+		#region Properties
+		[Range(1, int.MaxValue), NinjaScriptProperty]
+		[Display(ResourceType = typeof(Custom.Resource), Name = "Fast", GroupName = "NinjaScriptStrategyParameters", Order = 0)]
+		public int Fast
+		{ get; set; }
+
+		[Range(1, int.MaxValue), NinjaScriptProperty]
+		[Display(ResourceType = typeof(Custom.Resource), Name = "Slow", GroupName = "NinjaScriptStrategyParameters", Order = 1)]
+		public int Slow
+		{ get; set; }
+		#endregion
+	}
+}
